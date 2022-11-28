03f62e6a946d27e2e022996f1afeb83cb905f103
KaieChen <63878759+KaieChen@users.noreply.github.com>
2022-11-28 13:01:52 -0800

    Add files via upload

--- StudentAgent.py
+++ StudentAgent.py
@@ -35,10 +35,10 @@ class Student_Agent(object):
         self.model = NN()
         if version == 1:
             #23 was the last one without the noise
-            self.model.load_state_dict(torch.load('./THECNNSWAPFASTER169'))
+            self.model.load_state_dict(torch.load('./THECNNSWAPFASTER177'))
         elif version == 2:
             #4 was the last one without the noise
-            self.model.load_state_dict(torch.load('./THECNNSWAPFAST87'))
+            self.model.load_state_dict(torch.load('./THECNNSWAPFAST92'))
         else:
             self.model.load_state_dict(torch.load('./THECNNSWAP15'))
         self.board = None
